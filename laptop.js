869c9a7e2178dbde26456fe88168fc182a887dbc
M-Saleh-N <salehnasser65784@gmail.com>
2025-01-23 16:35:51 +0300

    pasted

---
+++ laptop.js
@@ -0,0 +1,30 @@
+// make an object for your laptop
+// key : valaue 
+let laptopObject = {
+    brand : "HP",
+    processor : 'Intel Core i5',
+    ram : '8gb',
+    colour : "black",
+    beii : 1000000,
+    used : true,
+    currency : "USH"
+    
+};
+console.log(laptopObject)
+
+//accessing the price using DOT NOTATION
+console.log(laptopObject.beii)
+
+//accessing the currency and price
+console.log(laptopObject.currency + " " + laptopObject.beii)
+
+//access the price to change
+laptopObject.beii = 800000
+console.log(laptopObject)
+
+//updating the name of the laptop
+laptopObject.brand = "Mac"
+console.log(laptopObject) 
+
+//Creating a Map
+const myMap = Map();
